cc47867f2e307bbb84884af98c53f91ab7a18294
jerryspann <jerryspann0@gmail.com>
2025-03-14 11:46:57 -0700

    added middleware for styling

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ const passUserToView = require('./middleware/pass-user-to-view.js');
 
 const port = process.env.PORT ? process.env.PORT : '3000';
 
+const path = require('path');
+
 mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on('connected', () => {
@@ -28,6 +30,7 @@ mongoose.connection.on('connected', () => {
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 // app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
